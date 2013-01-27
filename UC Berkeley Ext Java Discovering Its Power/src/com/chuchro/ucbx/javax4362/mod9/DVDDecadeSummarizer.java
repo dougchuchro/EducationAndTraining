df4cea7b4028625efbe39bd0ca57a4ecad7b09af
Doug Chuchro <doug@chuchro.net>
2013-01-27 18:15:20 +0900

    Version 1.0 complete, fully functional per specification

--- UC Berkeley Ext Java Discovering Its Power/src/com/chuchro/ucbx/javax4362/mod9/DVDDecadeSummarizer.java
+++ UC Berkeley Ext Java Discovering Its Power/src/com/chuchro/ucbx/javax4362/mod9/DVDDecadeSummarizer.java
@@ -1,8 +1,8 @@
 package com.chuchro.ucbx.javax4362.mod9;
 
 import javax.xml.stream.*;
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class DVDDecadeSummarizer {
 	public static void main(String[] args) {
@@ -11,59 +11,93 @@ public class DVDDecadeSummarizer {
 	}
 
 	public void read(String filepath) {
+		// This TreeMap will hold the decade summary count data until we write it to the output XML file
+		// The TreeMap key is the decade and the value is the count of DVDs released in that decade
+		TreeMap<Integer, Integer> decadeSummary = new TreeMap<Integer, Integer>();
+
+		// Open the input file, parse the XML and populate the TreeMap with the decade count summary info
 		try {
 			FileInputStream fileInputStream = new FileInputStream(filepath);
 			XMLInputFactory factory = XMLInputFactory.newInstance();
 			XMLStreamReader xmlStreamReader = factory.createXMLStreamReader(fileInputStream);
-			while (xmlStreamReader.hasNext()) {
-				readDVDList(xmlStreamReader);
-			}
+			decadeSummary = readDVDList(xmlStreamReader);
 			xmlStreamReader.close();
 		} catch (XMLStreamException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+		// Write the decade count summary TreeMap to an output XML file
+		try {
+			writeXMLDVDSummary(decadeSummary);
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private void readDVDList(XMLStreamReader reader) throws XMLStreamException {
+	private TreeMap<Integer, Integer> readDVDList(XMLStreamReader reader) throws XMLStreamException {
 		String name = "";
 		int releaseYear   = 0;
 		int releaseDecade = 0;
 		int count = 0;
 		TreeMap<Integer, Integer> decadeSummary = new TreeMap<Integer, Integer>();
 
-		int eventCode = reader.next();
-		switch (eventCode) {
-		case XMLStreamReader.START_ELEMENT:
-			String key = reader.getLocalName();
-			if (key.equals("DVD")) {
-				while (reader.hasNext()) {
-					eventCode = reader.next();
-					switch (eventCode) {
-					case XMLStreamReader.START_ELEMENT:
-						name = reader.getLocalName();
-						break;
-					case XMLStreamReader.CHARACTERS:
-						if (name.equals("release_year"))	{
-							releaseYear = Integer.parseInt(reader.getText());
-							releaseDecade = releaseYear/10 * 10;
-							System.out.println("Element  name= " + name + " value=" + releaseYear + " release decade= " + releaseDecade);
-							if (decadeSummary.containsKey(releaseDecade))	{
-								count = (int)decadeSummary.get(releaseDecade);
-								count++;
-							} else {
-								count = 1;
-							}
-							decadeSummary.put(releaseDecade,count);
-							System.out.println("B:Decade Summary: " + decadeSummary);
-						}
-						break;
+		while (reader.hasNext())	{
+			int eventCode = reader.next();
+			switch (eventCode) {
+			case XMLStreamReader.START_ELEMENT:
+				name = reader.getLocalName();
+				break;
+			case XMLStreamReader.CHARACTERS:
+				if (name.equals("release_year"))	{
+					releaseYear = Integer.parseInt(reader.getText());
+					// Calculate the release decade be subtracting the remainder of the 
+					// release year when divided by 10 from the release year itself
+					releaseDecade = releaseYear - (releaseYear%10);
+					// Determine if the current decade is already in the TreeMap
+					if (decadeSummary.containsKey(releaseDecade))	{
+						// if so, then increment the count
+						count = decadeSummary.get(releaseDecade);
+						count++;
+					} else {
+						// if not, this must be the first DVD of the decade so set the count to 1
+						count = 1;
 					}
+					decadeSummary.put(releaseDecade,count);
 				}
-				return;
+				break;
 			}
-			break;
+		}
+		return decadeSummary;
+	}
+
+	private void writeXMLDVDSummary(TreeMap<Integer, Integer> decadeSummary) throws XMLStreamException {
+		XMLStreamWriter xw =  null; 
+		XMLOutputFactory xof = XMLOutputFactory.newInstance(); 
+		try {
+			File file = new File("DVDSummary.xml");
+			FileWriter fw = new FileWriter(file);
+			xw =  xof.createXMLStreamWriter(fw);
+			xw.writeStartDocument("1.0");
+			xw.writeStartElement("DVD");
+				xw.writeStartElement("summary");
+					for (Map.Entry<Integer, Integer> entry : decadeSummary.entrySet())	{
+						xw.writeStartElement("count");
+						xw.writeAttribute("decade", entry.getKey().toString());
+						xw.writeCharacters(entry.getValue().toString());
+						xw.writeEndElement();
+					}
+				xw.writeEndElement();	// close the "summary" element
+			xw.writeEndElement();		// close the "DVD" element
+			xw.writeEndDocument();
+			xw.flush();
+			xw.close();
+			System.out.println("Wrote DVD Summary XML doc to : " + file.getAbsolutePath());
+		} catch  (XMLStreamException e) {
+			e.printStackTrace();
+		} catch (IOException e)  {
+			e.printStackTrace();
 		}
 	}
 
